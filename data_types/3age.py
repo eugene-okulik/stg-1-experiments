ffb4a675e177e8506308fe3930320bcbd1bc26ac
Eugeny Okulik <98970620+eugene-okulik@users.noreply.github.com>
2026-01-25 12:31:09 +0300

    Add files via upload

---
+++ data_types/3age.py
@@ -0,0 +1,9 @@
+# Эта программа рассчитывает год рождения
+
+print("Привет! Давай узнаем твой год рождения.")
+current_year = "2026"
+your_age = input("Сколько тебе лет? ")
+
+birth_year = current_year - your_age
+
+print("Ты родился в", birth_year, "году!")
---
+++ data_types/3pizza.py
@@ -0,0 +1,10 @@
+print("Мы заказали две пиццы.")
+slices_per_pizza = "8"
+number_of_pizzas = 2
+
+total_slices = slices_per_pizza * number_of_pizzas
+print("Итого у нас было", total_slices, "аппетитных кусочков.")
+
+friends = input("Сколько друзей пришло? ")
+eaten_slices = total_slices - friends
+print("После вечеринки осталось", eaten_slices, "кусочков.")
