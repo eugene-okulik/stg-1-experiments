be83b7fbe42a58936cd8bf454964ca9085f0c39b
ivan-kasianov <i.kaianovv@gmail.com>
2026-01-25 16:20:08 +0100

    changed the types of variables bill_amount and tip_percentage from str to float

--- data_types/3tips.py
+++ data_types/3tips.py
@@ -1,7 +1,7 @@
 # Эта программа рассчитывает сумму счета с чаевыми
 
-bill_amount = input("Введите сумму счета: ")
-tip_percentage = "15%"  # 15% чаевых
+bill_amount = float(input("Введите сумму счета: "))
+tip_percentage = 0.15  # 15% чаевых
 
 total_to_pay = bill_amount + (bill_amount * tip_percentage)
 
